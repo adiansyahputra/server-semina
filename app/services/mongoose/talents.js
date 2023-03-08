b6688354bd7eea3eb758481c0cf935e369d2ee46
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-03-08 11:16:23 +0700

    add model for orders api

--- app/services/mongoose/talents.js
+++ app/services/mongoose/talents.js
@@ -8,7 +8,7 @@ const { NotFoundError, BadRequestError } = require('../../errors');
 const getAllTalents = async (req) => {
   const { keyword } = req.query;
 
-  let condition = {};
+  let condition = { organizer: req.user.organizer };
 
   if (keyword) {
     condition = { ...condition, name: { $regex: keyword, $options: 'i' } };
@@ -34,12 +34,12 @@ const createTalents = async (req) => {
   await checkingImage(image);
 
   // cari talents dengan field name
-  const check = await Talents.findOne({ name });
+  const check = await Talents.findOne({ name, organizer: req.user.organizer });
 
   // apa bila check true / data talents sudah ada maka kita tampilkan error bad request dengan message pembicara duplikat
   if (check) throw new BadRequestError('pembicara sudah terdaftar');
 
-  const result = await Talents.create({ name, image, role });
+  const result = await Talents.create({ name, image, role, organizer: req.user.organizer });
 
   return result;
 };
@@ -47,7 +47,7 @@ const createTalents = async (req) => {
 const getOneTalents = async (req) => {
   const { id } = req.params;
 
-  const result = await Talents.findOne({ _id: id })
+  const result = await Talents.findOne({ _id: id, organizer: req.user.organizer })
     .populate({
       path: 'image',
       select: '_id name',
@@ -70,12 +70,13 @@ const updateTalents = async (req) => {
   const check = await Talents.findOne({
     name,
     _id: { $ne: id },
+    organizer: req.user.organizer,
   });
 
   // apa bila check true / data talents sudah ada maka kita tampilkan error bad request dengan message pembicara nama duplikat
   if (check) throw new BadRequestError('pembicara sudah terdaftar');
 
-  const result = await Talents.findOneAndUpdate({ _id: id }, { name, image, role }, { new: true, runValidators: true });
+  const result = await Talents.findOneAndUpdate({ _id: id }, { name, image, role, organizer: req.user.organizer }, { new: true, runValidators: true });
 
   // jika id result false / null maka akan menampilkan error `Tidak ada pembicara dengan id` yang dikirim client
   if (!result) throw new NotFoundError(`Tidak ada pembicara dengan id :  ${id}`);
@@ -88,6 +89,7 @@ const deleteTalents = async (req) => {
 
   const result = await Talents.findOne({
     _id: id,
+    organizer: req.user.organizer,
   });
 
   if (!result) throw new NotFoundError(`Tidak ada pembicara dengan id :  ${id}`);
